3a4b3cd0c979a9824ca485a01d76e09740364c3e
Lyubitskiy Maxim <root@DESKTOP-BK46MSC>
2023-11-10 19:53:09 +0700

    first part

--- vk/1.py
+++ vk/1.py
@@ -1,4 +1,5 @@
 import vk_api
+import json
 from config import *
 
 # https://oauth.vk.com/authorize? // запрос на получение access token
@@ -9,7 +10,18 @@ from config import *
 # &response_type=token
 # &v=5.21
 
+def groupInfo(session ,groupId):
+    return session.method("groups.getById", {"group_id": groupId, "fields" : "activity,ban_info,can_post,can_see_all_posts,city,\
+                                             contacts,counters,country,cover,description,finish_date,fixed_post,links,market,members_count,\
+                                             place,site,start_date,status,verified,wiki_page"})
+
+def writeJson(info, filename):
+    with open(filename, 'w', encoding='utf8') as file:
+        json.dump(info[0], file, separators=(',', ':'), indent=4, ensure_ascii=False)
+
 if __name__ == "__main__":
     session = vk_api.VkApi(token=token)
-    userStatus = session.method("status.get", {"user_id": 546543569})
-    print(userStatus)
+    
+    info = groupInfo(session, 215278139)
+    writeJson(info, "215278139.json")
+
