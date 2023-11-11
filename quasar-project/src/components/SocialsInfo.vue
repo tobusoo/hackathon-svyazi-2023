6a534e10b996c5238024ec507916747148a56a45
Dmitryuk Vyacheslav <dmitryuk.slavik@yandex.ru>
2023-11-11 20:37:41 +0700

    Add Web-UI

---
+++ quasar-project/src/components/SocialsInfo.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="q-pa-md q-gutter-sm">
+    <div class="avatar-card">
+      <q-avatar size="120px" font-size="52px" color="teal" text-color="white" icon="warning" />
+      <div class="name-desc">
+        <p class="name"> Имярек Имярек </p>
+        <p class="desc"> Описание </p>
+        <q-btn color="primary" icon="web" label="На страницу" />
+      </div>
+    </div>
+  </div>
+  <!--
+  <div class="q-pa-md q-gutter-md">
+    <q-list bordered class="rounded-borders" style="max-width: 350px">
+      <q-item-label header>Друзья</q-item-label>
+
+      <q-item clickable v-ripple>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/avatar2.jpg">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label lines="1">Brunch this weekend?</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator inset="item" />
+
+      <q-item clickable v-ripple>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/avatar4.jpg">
+          </q-avatar>
+        </q-item-section>
+        <q-item-section>
+          <q-item-label lines="1">Linear Project</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </div>
+  -->
+</template>
+
+
+<script>
+import { defineComponent, ref } from 'vue'
+export default defineComponent({
+  name: 'SocialsInfo',
+  setup() {
+    const itemsRef = ref([{}, {}, {}, {}, {}, {}, {}])
+    const itemsId = ref([{}, {}, {}, {}, {}, {}, {}])
+    const scrollTargetRef = ref(null)
+
+    return {
+      itemsRef,
+      itemsId,
+      scrollTargetRef,
+
+      onLoadRef(index, done) {
+        setTimeout(() => {
+          itemsRef.value.push({}, {}, {}, {}, {}, {}, {})
+          done()
+        }, 2000)
+      },
+
+      onLoadId(index, done) {
+        setTimeout(() => {
+          itemsId.value.push({}, {}, {}, {}, {}, {}, {})
+          done()
+        }, 2000)
+      }
+    }
+  }
+});
+</script>
+
+<style>
+.avatar-card {
+  display: flex;
+  gap: 1rem;
+}
+
+.name-desc {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+p {
+  margin-bottom: 0%;
+}
+
+.name {
+  flex: 1 0 auto;
+  font-size: 32px;
+}
+
+.desc {
+  flex: 2 0 auto;
+}
+</style>
+
---
+++ quasar-project/src/pages/VkQuery.vue
@@ -0,0 +1,73 @@
+<template>
+  <DataEntrance style="height: 240px" icon="group" ttitle="Поиск">
+    <div class="FormCollection">
+      <div class="topform">
+        <q-input class="topform__input" outlined type="number" v-model.number="userID" label="ID пользователя" />
+        <q-btn square color="primary" icon="search" label="Поиск пользователя" />
+      </div>
+      <div class="topform">
+        <q-input class="topform__input" outlined type="number" v-model.number="groupID" label="ID группы" />
+        <q-btn square color="primary" icon="search" label="Поиск группы" />
+      </div>
+    </div>
+  </DataEntrance>
+  <DataEntrance style="height: 61vh" icon="man" ttitle="Информация">
+    <SocialsInfo> </SocialsInfo>
+  </DataEntrance>
+</template>
+
+
+<script>
+import DataEntrance from "../components/DataEntrance.vue"
+import SocialsInfo from "../components/SocialsInfo.vue"
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  components: { DataEntrance, SocialsInfo },
+  setup() {
+    const userID = ref(0);
+    const groupID = ref(0);
+    return {
+      userID,
+      groupID
+    };
+  }
+});
+</script>
+
+<style>
+.FormCollection {
+  display: inline-flex;
+  flex-direction: row;
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+}
+
+.topform {
+  display: flex;
+  gap: 1rem;
+  background-color: lightblue;
+  padding: 2rem;
+  align-items: stretch;
+  flex-grow: 2;
+}
+
+.topform__input {
+  background-color: white;
+  border-radius: 5%;
+}
+
+.deflx {
+  display: flex;
+  flex-direction: row;
+  justify-items: stretch;
+
+}
+
+.bottom_box {
+  flex: 0 1 calc(50% - 0px);
+  height: 65vh;
+}
+</style>
+
