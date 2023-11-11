2cec05dc57ce0ec8328bceaa775a509dccaa131f
Lyubitskiy Maxim <root@DESKTOP-BK46MSC>
2023-11-11 19:09:37 +0700

    add comments

--- vk/main.py
+++ vk/main.py
@@ -2,20 +2,15 @@ import vk_api
 import json
 from config import *
 
-# https://oauth.vk.com/authorize? // запрос на получение access token
-# client_id=51789951               // Надо просто скопировать ссылку и вставить в браузер
-# &display=page
-# &redirect_url=https://oauth.vk.com/blank.html
-# &scope=offline,friends,wall,status,docs,groups
-# &response_type=token
-# &v=5.21
+# https://oauth.vk.com/authorize?&display=page&redirect_url=https://oauth.vk.com/blank.html&scope=offline,friends,wall,status,docs,groups&response_type=token&v=5.21
+# Запрос на access token
 
-def getGroupInfo(session: vk_api.VkApi, groupId: str):
+def getGroupInfo(session: vk_api.VkApi, groupId: str): # Задание 2.1
     return session.method("groups.getById", {"group_id": groupId, "fields" : "activity,ban_info,can_post,can_see_all_posts,city,\
                                              contacts,counters,country,cover,description,finish_date,fixed_post,links,market,members_count,\
                                              place,site,start_date,status,verified,wiki_page"})
 
-def getUsersInfo(session: vk_api.VkApi, userIds: str):
+def getUsersInfo(session: vk_api.VkApi, userIds: str): # Задание 2.2
     return session.method("users.get", {"user_ids": userIds, "fields": "activities,about,blacklisted,blacklisted_by_me,books,\
                                         bdate,can_be_invited_group,can_post,can_see_all_posts,can_see_audio,can_send_friend_request,\
                                         can_write_private_message,career,common_count,connections,contacts,city,country,crop_photo,\
@@ -25,7 +20,7 @@ def getUsersInfo(session: vk_api.VkApi, userIds: str):
                                         movies,music,nickname,occupation,online,personal,photo_id,photo_max,photo_max_orig,quotes,\
                                         relation,relatives,timezone,tv,universities"})
 
-def getFullWall(session: vk_api.VkApi, owner_id: int):
+def getFullWall(session: vk_api.VkApi, owner_id: int): # Возвращает словарь с количеством записей и списком записей
     tools = vk_api.VkTools(session)
     wall = tools.get_all('wall.get', 100, {'owner_id': owner_id})
     print('Posts count:', wall['count'])
@@ -111,7 +106,7 @@ def captcha_handler(captcha):
 
 
 def twoFactorAuth(session: vk_api.VkApi):
-    # """ Пример обработки двухфакторной аутентификации """ (Y)
+    # """ Пример обработки двухфакторной аутентификации """
 
     try:
         session.auth(token_only=True)
